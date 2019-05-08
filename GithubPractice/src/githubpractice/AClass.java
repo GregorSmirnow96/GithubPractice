cecd5884f8f4ffa87c5f5d637038e552f0093fab
GregorSmirnow96 <GregorSmirnow96@gmail.com>
2019-05-08 12:38:58 -0600

    Adding java project.

---
+++ GithubPractice/src/githubpractice/AClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubpractice;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ *
+ * @author im5no
+ */
+public class AClass
+{
+    public int aFunction()
+    {
+        throw new NotImplementedException();
+    }
+}
